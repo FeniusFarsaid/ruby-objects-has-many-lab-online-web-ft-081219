4ac7a0c187743f7d981b5df14c80c9ac046b0aba
David Ingram <david@ingramaviation.com>
2019-08-27 19:44:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,10 @@ class Artist
     
   end
   
+  def songs
+    Song.all.select{|song| song.artist == self}
+  end
+  
   def add_song_by_name(name)
     song = Song.new(name)
     self.songs << song
