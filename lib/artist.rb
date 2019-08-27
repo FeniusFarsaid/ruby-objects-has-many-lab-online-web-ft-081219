8bb6734c5617fedbf695eabad8b82c4272037545
David Ingram <david@ingramaviation.com>
2019-08-27 19:45:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist 
-  attr_accessor :name, :songs 
+  attr_accessor :name 
   
   @@song_count = 0
   
@@ -11,7 +11,6 @@ class Artist
   def add_song(song)
     #@songs << song
     song.artist = self
-    
   end
   
   def songs
